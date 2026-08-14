f7d6463727b6d3c9ea3e92e586a9f9e65b80d338
DylanEggs <ranchodakota@yahoo.com>
2026-08-14 07:58:03 -0400

    Load hardened sync authority

--- flock-photo-fix-v2.js
+++ flock-photo-fix-v2.js
@@ -1,4 +1,4 @@
-import("./sync-authority-v2.js?v=1")
+import("./sync-authority-v2.js?v=2")
   .then(async () => { if (window.EggSyncAuthorityReady) await window.EggSyncAuthorityReady(); })
   .then(() => import("./bird-photo-service-v4.js?v=2"))
   .then(() => import("./flock-manager-v7.js?v=4"))
