098ec8d6f7df86b27046cfe36169112ca2fae66e
DylanEggs <ranchodakota@yahoo.com>
2026-08-16 17:11:53 -0400

    Use storage recovery build 1710 in app loader

--- app2.js
+++ app2.js
@@ -1,6 +1,6 @@
 (() => {
   "use strict";
-  const BUILD = String(window.__ChickenEggsBuild || "20260816-1700");
+  const BUILD = String(window.__ChickenEggsBuild || "20260816-1710");
   const load = (src, module = false) => document.write(`<script${module?' type="module"':''} src="${src}?v=${encodeURIComponent(BUILD)}"><\/script>`);
 
   load("sync-safety-preload-v1.js");
